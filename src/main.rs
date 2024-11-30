bfa32e26ace97efe52f2eaeed13fff3bd15e4da6
Timon Vonk <timonv@gmail.com>
2024-11-30 12:15:43 +0100

    Convert to rust 2021

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
-extern crate vader_sentiment;
+// extern crate vader_sentiment;
+
+// fn main() {
+//     vader_sentiment::demo::run_demo();
+// }
+
+use clap::Parser;
+use vader_sentiment::SentimentIntensityAnalyzer;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    text: String,
+}
 
 fn main() {
-    vader_sentiment::demo::run_demo();
+    let args = Args::parse();
+
+    let analyzer = SentimentIntensityAnalyzer::new();
+    let scores = analyzer.polarity_scores(&args.text);
+
+    println!("Polarity scores");
+    println!("{:#?}", scores);
 }
